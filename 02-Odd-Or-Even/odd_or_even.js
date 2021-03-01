59cb3dcf3770b5da5ab3e91827f89143ea85ab16
Grace Kabeya <antoinefaith1@gmail.com>
2021-03-01 13:42:28 +0100

    finished

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -1,7 +1,37 @@
 // odd or even reporter
+/*var i;
+for (i = 0; i < 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i + "is a even")
+  } else {
+    console.log(i +"is a odd")
+  }
+}
+;*/
+var text = '';
+var num = 0
+/*while (num < 20){
+  text += + num; num++;
+  if (num % 2 == 0) {
+    console.log(num +" "+ "is a even")
+  } else {
+    console.log(num +" "+ "is a odd")
+  }
+};*/
 
-// write a for loop to iterate from 0 to 20
-
+do {
+  text += + num; num++;
+  if (num % 2 == 0) {
+    console.log(num +" "+ "is a even")
+  } else {
+    console.log(num +" "+ "is a odd")
+  }
+} while (num < 20);
+// write a for loop to iterate from 0 to 20;
+for (var i = 0 ; i < 20; i++){
+  console.log(i)
+  }
+  ;
   // check if the current is even or odd
 
   // report the result to the screen
